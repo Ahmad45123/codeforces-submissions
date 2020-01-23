920bcad718c12415e7dc3a7caf6d263df0400150
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-01-23 20:39:00 +0200

    Add solution for problem `B - Burglar and Matches` Link: https://codeforces.com/contest/16/problem/B Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/16/submission/69435050

--- codeforces/16/B.cpp
+++ codeforces/16/B.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 
 bool compare(pair<int, int> a, pair<int, int> b) {
-    return a.second > b.second;
+    return a.second < b.second;
 }
 
 int main()
@@ -17,14 +17,16 @@ int main()
         list[i] = {a, b};
     }
     sort(list.begin(), list.end(), compare);
-    //reverse(list.begin(), list.end());
+    reverse(list.begin(), list.end());
     
     int ans = 0;
     int cur = 0;
     for(int i = 0; i < m; i++) {
-        int amnt = min(cur+list[i].first, n);
-        ans += list[i].second*(amnt-cur);
-        cur += (amnt-cur);
+        while(cur+1 <= n && list[i].first > 0) {
+            cur ++;
+            ans += list[i].second;
+            list[i].first --;
+        }
     }
 
     cout << ans << "\n";
