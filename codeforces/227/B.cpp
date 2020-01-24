31b78fdf598d31cb4c497e0842a7db38ce38ff33
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-01-24 14:28:00 +0200

    Add solution for problem `B - Effective Approach` Link: https://codeforces.com/contest/227/problem/B Tags: implementation, *1100 Ref: https://codeforces.com/contest/227/submission/69464973

--- codeforces/227/B.cpp
+++ codeforces/227/B.cpp
@@ -7,7 +7,7 @@ int main()
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     
     int n; cin >> n;
-    unordered_map<int, int> numbers;
+    map<int, int> numbers;
     for(int i = 0; i < n; i++) {
         int tmp; cin >> tmp;
         numbers[tmp] = i;
