d269b38535be5a5ddbf8ead6c2e193ecd1f17239
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-03-03 22:33:00 +0200

    Add solution for problem `A - Minutes Before the New Year` Link: https://codeforces.com/contest/1283/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1283/submission/72370832

---
+++ codeforces/1283/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    
+    int t; cin >> t;
+    while(t--) {
+        int h,m; cin >> h >> m;
+        cout << (24-h)*60-m << "\n";
+    }
+    
+    return 0;
+}
