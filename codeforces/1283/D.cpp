e4bf37bab99a4983f111afb3139e127ec307ebb5
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-03-04 23:31:00 +0200

    Add solution for problem `D - Christmas Trees` Link: https://codeforces.com/contest/1283/problem/D Tags: graphs, greedy, shortest paths, *1800 Ref: https://codeforces.com/contest/1283/submission/72474247

--- codeforces/1283/D.cpp
+++ codeforces/1283/D.cpp
@@ -21,7 +21,7 @@ bool operator > (const tree& lhs, const tree& rhs)
 
 int main()
 {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    //ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     
     int n, m; cin >> n >> m;
     vector<int> tmpTrees(n);
