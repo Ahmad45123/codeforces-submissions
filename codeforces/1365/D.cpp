3c1cd39aaf6b458d9d0c0b1bcdf670d0f64371cf
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-06-07 21:20:00 +0200

    Add solution for problem `D - Solve The Maze` Link: https://codeforces.com/contest/1365/problem/D Tags: constructive algorithms, dfs and similar, dsu, graphs, greedy, implementation, shortest paths, *1700 Ref: https://codeforces.com/contest/1365/submission/82883583

--- codeforces/1365/D.cpp
+++ codeforces/1365/D.cpp
@@ -3,28 +3,6 @@ using namespace std;
 
 typedef long long ll;
 
-vector<vector<char>> arr;
-bool vis[51][51];
-
-int countGs(int i, int j, int n, int m) {
-    if(vis[i][j])
-        return 0;
-    if(arr[i][j] == '#')
-        return 0;
-    vis[i][j] = true;
-
-    int cnt = (arr[i][j] == 'G' ? 1 : 0);
-    if(i+1 <= n)
-        cnt += countGs(i+1, j, n, m);
-    if(i-1 >= 0)
-        cnt += countGs(i-1, j, n, m);
-    if(j+1 <= m)
-        cnt += countGs(i, j+1, n, m);
-    if(j-1 >= 0)
-        cnt += countGs(i, j-1, n, m);
-    return cnt;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -32,7 +10,7 @@ int main()
     int t; cin >> t;
     while(t--) {
         int n, m; cin >> n >> m;
-        arr = vector<vector<char>>(n, vector<char>(m));
+        vector<vector<char>> arr(n, vector<char>(m));
         int nmgood = 0;
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++) {
@@ -79,8 +57,36 @@ int main()
             continue;
         }
 
+        bool vis[51][51];
         memset(vis, false, sizeof vis);
-        int gsfound = countGs(n-1, m-1, n-1, m-1);
+        int gsfound = 0;
+        queue<pair<int, int>> q;
+        q.push({n-1, m-1});
+        while(!q.empty()) {
+            pair<int, int> itm = q.front();
+            int i = itm.first;
+            int j = itm.second;
+            q.pop();
+            if(vis[i][j])
+                continue;
+            vis[i][j] = true;
+            if(arr[i][j] == 'G') {
+                gsfound ++;
+            } else if(arr[i][j] == '#')
+                continue;
+            if(i+1 < n && arr[i+1][j] != '#' && !vis[i+1][j]) {
+                q.push({i+1, j});
+            }
+            if(i-1 >= 0 && arr[i-1][j] != '#' && !vis[i-1][j]) {
+                q.push({i-1, j});
+            }
+            if(j+1 < m && arr[i][j+1] != '#' && !vis[i][j+1]) {
+                q.push({i, j+1});
+            }
+            if(j-1 >= 0 && arr[i][j-1] != '#' && !vis[i][j-1]) {
+                q.push({i, j-1});
+            }
+        }
         
         cout << (gsfound == nmgood ? "Yes\n" : "No\n");
     }
