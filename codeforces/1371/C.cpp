7fb3cd0542d368f344315b0c0ae852b3e7f8d9b5
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-07-23 22:43:00 +0200

    Add solution for problem `C - A Cookie for You` Link: https://codeforces.com/contest/1371/problem/C Tags: greedy, implementation, math, *1300 Ref: https://codeforces.com/contest/1371/submission/87810136

--- codeforces/1371/C.cpp
+++ codeforces/1371/C.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 typedef long long ll;
- 
+
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -10,15 +10,12 @@ int main()
     int t; cin >> t;
     while(t--) {
         ll a, b, n, m; cin >> a >> b >> n >> m;
-        if(n+m > a+b) {
+        ll mx = max(a, b);
+        ll mn = min(a, b);
+        if(n+m > a+b || m > mn) {
             cout << "No\n";
         } else {
-            ll mx = max(a, b);
-            ll mn = min(a, b);
-            if(m <= mn)
-                cout << "Yes\n";
-            else
-                cout << "No\n";
+            cout << "Yes\n";
         }
     }
     
