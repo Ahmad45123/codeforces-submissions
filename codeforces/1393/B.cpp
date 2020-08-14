ae10a9188faf2030d092d1c647b5887f22614297
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-08-14 19:42:00 +0200

    Add solution for problem `B - Applejack and Storages` Link: https://codeforces.com/contest/1393/problem/B Tags: constructive algorithms, data structures, greedy, implementation, *1400 Ref: https://codeforces.com/contest/1393/submission/89962504

--- codeforces/1393/B.cpp
+++ codeforces/1393/B.cpp
@@ -12,9 +12,13 @@ int main()
     vector<int> mp(100001);
     for(int i = 0; i < n; i++) {
         int x ; cin >> x;
+        eights -= mp[x] / 8;
+        sixes -= mp[x] / 6;
         fours -= mp[x] / 4;
         twos -= mp[x] / 2;
         mp[x] ++;
+        eights += mp[x] / 8;
+        sixes += mp[x] / 6;
         fours += mp[x] / 4;
         twos += mp[x] / 2;
     }
@@ -23,7 +27,8 @@ int main()
     int q; cin >> q;
     while(q--) {
         char o; int v; cin >> o >> v;
-
+        eights -= mp[v] / 8;
+        sixes -= mp[v] / 6;
         fours -= mp[v] / 4;
         twos -= mp[v] / 2;
             
@@ -34,10 +39,23 @@ int main()
             mp[v] --;
         }
 
+        eights += mp[v] / 8;
+        sixes += mp[v] / 6;
         fours += mp[v] / 4;
         twos += mp[v] / 2;
 
-        cout << (fours >= 1 && twos >= 4 ? "YES\n" : "NO\n");
+        bool good = false;
+        if(eights >= 1) {
+            good = true;
+        } else if(sixes >= 1 && twos >= 5) {
+            good = true;
+        } else if(fours >= 2) {
+            good = true;
+        } else if(fours >= 1 && twos >= 4) {
+            good = true;
+        }
+
+        cout << (good ? "YES\n" : "NO\n");
     }
     
     return 0;
