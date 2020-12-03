79104d339a7e6ed0faa7f9790d5432014f2a1b16
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-12-03 17:56:00 +0200

    Add solution for problem `D - Sequence and Swaps` Link: https://codeforces.com/contest/1455/problem/D Tags: dp, greedy, sortings, *1600 Ref: https://codeforces.com/contest/1455/submission/100288141

--- codeforces/1455/D.cpp
+++ codeforces/1455/D.cpp
@@ -3,26 +3,6 @@ using namespace std;
 
 typedef long long ll;
 
-vector<int> arr;
-vector<vector<vector<int>>> dp;
-
-int solve(int i, int prev, int cx) {
-    if(i >= arr.size())
-        return 0;
-
-    if(~dp[i%2][prev][cx])
-        return dp[i%2][prev][cx];
-
-    int ans = 1e9;
-
-    if(prev <= arr[i])
-        ans = min(ans, solve(i+1, arr[i], cx));
-    if(prev <= cx && cx < arr[i])
-        ans = min(ans, 1+solve(i+1, cx, arr[i]));
-    
-    return dp[i%2][prev][cx] = ans;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -30,21 +10,38 @@ int main()
     int t; cin >> t;
     while(t--) {
         int n, x; cin >> n >> x;
-        arr = vector<int>(n);
-        dp = vector<vector<vector<int>>>(2, vector<vector<int>>(501, vector<int>(501, -1)));
+        vector<int> arr(n);
         for(int &i : arr)
             cin >> i;
+        
+        int ans = 0;
+        while(true) {
+            bool sorted = true;
+            for(int i = 1; i < n; i++)
+                if(arr[i] < arr[i-1]) {
+                    sorted = false;
+                }
+            if(sorted)
+                break;
 
-        if(n == 1) {
-            cout << "0\n";
-            continue;
+            int pos = -1;
+            for(int i = 0; i < n; i++) {
+                if(arr[i] > x) {
+                    pos = i;
+                    break;
+                }
+            }
+            
+            if(pos == -1) {
+                ans = -1;
+                break;
+            } else {
+                swap(arr[pos], x);
+                ans ++;
+            }
         }
 
-        int ans = solve(1, arr[0], x);
-        if(arr[0] > x)
-            ans = min(ans, 1+solve(1, x, arr[0]));
-
-        cout << (ans == 1e9 ? -1 : ans) << "\n";
+        cout << ans << "\n";
     }
     
     return 0;
