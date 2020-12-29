8bfec4ffd0c714031040212dc92235ed5132580e
Ahmed Mamdouh <ahmad.alwasifi@gmail.com>
2020-12-29 15:04:00 +0200

    Add solution for problem `A - Regular Bracket Sequence` Link: https://codeforces.com/contest/1469/problem/A Tags: constructive algorithms, greedy, *1000 Ref: https://codeforces.com/contest/1469/submission/102689875

---
+++ codeforces/1469/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    
+    int t; cin >> t;
+    while(t--) {
+        string s; cin >> s;
+        cout << (s.size() % 2 != 0 || s[0] == ')' || s[s.size()-1] == '(' ? "NO\n" : "YES\n");
+    }
+    
+    return 0;
+}
